dd9cf57ca3dba6f30988965387e3b39097849a25
Lorenzo Boccalon <883125@stud.unive.it>
2020-11-11 18:01:03 +0100

    Added Apostolico Chrochemore fun.

--- kmp.py
+++ kmp.py
@@ -1,30 +1,3 @@
-"""
-algorithm kmp_table:
-    input:
-        an array of characters, W (the word to be analyzed)
-    output:
-        an array of integers, T (the table to be filled)
-
-    define variables:
-        an integer, pos ← 1 (the current position we are computing in T)
-        an integer, cnd ← 0 (the zero-based index in W of the next character of the current candidate substring)
-
-    let T[0] ← -1
-
-    while pos < length(W) do
-        if W[pos] = W[cnd] then
-            let T[pos] ← T[cnd]
-        else
-            let T[pos] ← cnd
-            let cnd ← T[cnd] (to increase performance)
-            while cnd ≥ 0 and W[pos] ≠ W[cnd] do
-                let cnd ← T[cnd]
-        let pos ← pos + 1, cnd ← cnd + 1
-
-    let T[pos] ← cnd (only needed when all word occurrences are searched)
-"""
-
-
 def kmp_table(pattern) -> [int]:
     m = len(pattern)
     table = [-1] * m
@@ -47,42 +20,10 @@ def kmp_table(pattern) -> [int]:
     return table
 
 
-"""
-algorithm kmp_search:
-    input:
-        an array of characters, S (the text to be searched)
-        an array of characters, W (the word sought)
-    output:
-        an array of integers, P (positions in S at which W is found)
-        an integer, nP (number of positions)
-
-    define variables:
-        an integer, j ← 0 (the position of the current character in S)
-        an integer, k ← 0 (the position of the current character in W)
-        an array of integers, T (the table, computed elsewhere)
-
-    let nP ← 0
-
-    while j < length(S) do
-        if W[k] = S[j] then
-            let j ← j + 1
-            let k ← k + 1
-            if k = length(W) then
-                (occurrence found, if only first occurrence is needed, m ← j - k  may be returned here)
-                let P[nP] ← j - k, nP ← nP + 1
-                let k ← T[k] (T[length(W)] can't be -1)
-        else
-            let k ← T[k]
-            if k < 0 then
-                let j ← j + 1
-                let k ← k + 1
-"""
-
-
 def kmp_search(text, pattern) -> [int]:
     j, k = 0, 0  # the position of the current character in text, the position of the current character in pattern
     table = kmp_table(pattern)
-    p = []
+    pos = []
 
     while j < len(text):
         if pattern[k] == text[j]:
@@ -90,16 +31,57 @@ def kmp_search(text, pattern) -> [int]:
             k += 1
             if k == len(pattern):
                 # match
-                p.append(j - k)
+                pos.append(j - k)
                 k = table[k]
         else:
             k = table[k]
             if k < 0:
                 j += 1
                 k += 1
-    return p
+    return pos
 
 
-text, pattern = "bbbabbbba", "bbba"
-print("Table of pattern", pattern, ":", kmp_table(pattern))
-print("The pattern occurs at positions :", kmp_search(text, pattern))
+def axamac(pattern, text) -> [int]:
+    pos = []
+
+    # preprocessing
+    table = kmp_table(pattern)
+    m, n = len(pattern), len(text)
+    ell = 1
+    while pattern[ell - 1] == pattern[ell]:
+        ell += 1
+        if ell == m:
+            ell = 0
+
+    # searching
+    i = ell
+    j, k = 0, 0
+    while j <= n - m:
+        while i < m and pattern[i] == text[i + j]:
+            i += 1
+        if i >= m:
+            while k < ell and pattern[k] == text[i + j]:
+                k += 1
+            if k >= ell:
+                pos.append(j)
+        j += i - table[i]
+        if i == ell:
+            k = max(0, k - 1)
+        else:
+            if table[i] <= ell:
+                k = max(0, table[i])
+                i = ell
+            else:
+                k = ell
+                i = table[i]
+
+
+# t, p = "bbbabbbba", "bbba"
+t, p = "GCATCGCAGAGAGTATACAGTACG", "GCAGAGAG"
+# pre processing
+print("Table of pattern", p, ":", kmp_table(p))
+# KMP
+print("The pattern occurs at positions :", kmp_search(t, p))
+# Apostolico-Chrochemore
+print("The pattern occurs at positions :", kmp_search(t, p))
+
